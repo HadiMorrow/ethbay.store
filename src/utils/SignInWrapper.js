b96e4d2fa7b8f93958cc3748fd1b9e4e334d807c
Hadi Morrow <hadi.a.morrow@gmail.com>
2022-07-30 02:12:22 -0400

    Changes for heroku build done

--- src/utils/SignInWrapper.js
+++ src/utils/SignInWrapper.js
@@ -1,6 +1,5 @@
-import {
-    Navigate,
-} from "react-router-dom";
+// import { Navigate } from "react-router-dom";
+import React, { Navigate }  from "react";
 
 export const SignInWrapper = ({ children, currentAccount, adminAddress }) => {
     console.log(currentAccount == adminAddress)
